20c13fd880100c9f0ad9a13f853f0fc20457f3ae
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-02 10:33:53 +0530

    My first LINQ query

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,10 +1,28 @@
-﻿
+﻿using System;
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            //LINQ Syntax
+            var query = from c
+                        in context.Courses
+                        where c.Name.Contains("C#")
+                        orderby c.Name
+                        select c;
+
+            foreach(Course c in query)
+            {
+                Console.WriteLine(c.Name);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
