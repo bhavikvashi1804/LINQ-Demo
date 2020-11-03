1d6cf283e231a3a721e7c5dd2e23e4c3b42c8e83
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-03 13:22:29 +0530

    Adding a new object: way 3: do not use this

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -9,17 +9,23 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            
-            
+
+            var author = new Author
+            {
+                Id = 1,
+                Name = "Mosh Hamedani"
+            };
+
             var context = new PlutoContext();
+            context.Authors.Attach(author);
 
             //create a new course
             var course = new Course {
-                Name = "Flutter Course 3",
+                Name = "Flutter Course 4",
                 Description = "flutter.dev",
                 FullPrice = 120,
                 Level = 1,
-                AuthorId = 1
+                Author = author
             };
 
             //add to the context it saves in local memory
