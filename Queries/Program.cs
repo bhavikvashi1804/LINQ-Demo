660c465340b9180b6b8ef0994cfded5edb5370c4
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-03 13:17:22 +0530

    Adding a new object: way 2 : use foreign key

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -9,24 +9,22 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            
+            
             var context = new PlutoContext();
-
-            //all authors
-            var authors = context.Authors.ToList();
-            //author with ID == 1
-            var author = context.Authors.SingleOrDefault(a => a.Id == 1);
 
             //create a new course
             var course = new Course {
-                Name = "Flutter Course 2",
+                Name = "Flutter Course 3",
                 Description = "flutter.dev",
                 FullPrice = 120,
                 Level = 1,
-                Author = author
+                AuthorId = 1
             };
 
-            context.Courses.Add(course);
             //add to the context it saves in local memory
+            context.Courses.Add(course);
+           
 
             //now make the changes into the database
             context.SaveChanges();
