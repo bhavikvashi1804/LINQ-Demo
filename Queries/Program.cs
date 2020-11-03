98005c8a9b7e00d3891ca04e677437fc6438cd79
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-03 13:34:45 +0530

    Update an existing object

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,27 +10,16 @@ namespace Queries
         static void Main(string[] args)
         {
 
-            var author = new Author
-            {
-                Id = 1,
-                Name = "Mosh Hamedani"
-            };
-
+            //get the context
             var context = new PlutoContext();
-            context.Authors.Attach(author);
-
-            //create a new course
-            var course = new Course {
-                Name = "Flutter Course 4",
-                Description = "flutter.dev",
-                FullPrice = 120,
-                Level = 1,
-                Author = author
-            };
-
-            //add to the context it saves in local memory
-            context.Courses.Add(course);
            
+            //get the course with ID==4
+            var course = context.Courses.Find(4);
+            
+            //update the course
+            course.Name = "JS Course";
+            course.AuthorId = 1;
+            
 
             //now make the changes into the database
             context.SaveChanges();
